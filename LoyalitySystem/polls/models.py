d42861abb03286f0d3f79ae976078242c7264dad
M-Kulinkovich <50986910+M-Kulinkovich@users.noreply.github.com>
2023-02-19 22:45:25 +0300

    Update models.py

--- LoyalitySystem/polls/models.py
+++ LoyalitySystem/polls/models.py
@@ -39,7 +39,6 @@ class Cards(models.Model):
 
 class Orders(models.Model):
     card_id = models.ForeignKey(Cards, on_delete=models.CASCADE, related_name='orders')
-    # number_oder = models.AutoField(primary_key=True)
     date_order = models.DateTimeField(auto_now_add=True)
     sum_order = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     discount_percent = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
